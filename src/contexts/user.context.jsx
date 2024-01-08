65e92611438c5d784c3daf3ba69413c5c0b3425f
Pratyush Bhatnagar <38534434+pratyushbh@users.noreply.github.com>
2024-01-08 11:54:48 +0530

    created checkoutpage and cart functionality

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -4,8 +4,6 @@ import { createContext,useState } from "react";
 export const UserContext=createContext({
     currentUser:null,
     setCurrentUser:()=>null,
-    email:null,
-    setEmail:()=>null,
 });
 
 export const UserProvider=({children})=>{
